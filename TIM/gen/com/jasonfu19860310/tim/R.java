b0c8c67654b0d241899b1122777cb4dd9d509ada
jason <fuqiang.dev@gmail.com>
2013-07-25 22:36:14 +0800

    refine create project page

--- TIM/gen/com/jasonfu19860310/tim/R.java
+++ TIM/gen/com/jasonfu19860310/tim/R.java
@@ -24,14 +24,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int AddProject=0x7f080007;
-        public static final int DelProject=0x7f080006;
-        public static final int datePicker1=0x7f080002;
-        public static final int datePicker2=0x7f080004;
-        public static final int project_list=0x7f080005;
-        public static final int scrollView1=0x7f080000;
+        public static final int AddProject=0x7f08001a;
+        public static final int DelProject=0x7f080019;
+        public static final int button1=0x7f08000b;
+        public static final int button2=0x7f08000d;
+        public static final int button3=0x7f080017;
+        public static final int checkBox1=0x7f080010;
+        public static final int checkBox2=0x7f080011;
+        public static final int checkBox3=0x7f080012;
+        public static final int checkBox4=0x7f080013;
+        public static final int checkBox5=0x7f080014;
+        public static final int checkBox6=0x7f080015;
+        public static final int checkBox7=0x7f080016;
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f080007;
+        public static final int editText3=0x7f080009;
+        public static final int horizontalScrollView1=0x7f080005;
+        public static final int project_list=0x7f080018;
         public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080003;
+        public static final int textView10=0x7f08000a;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080006;
+        public static final int textView4=0x7f080008;
+        public static final int textView5=0x7f08000c;
+        public static final int textView6=0x7f08000e;
+        public static final int textView7=0x7f08000f;
+        public static final int textView8=0x7f080000;
+        public static final int textView9=0x7f080003;
     }
     public static final class layout {
         public static final int activity_create_project=0x7f030000;
@@ -42,13 +61,35 @@ public final class R {
         public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int Friday=0x7f050011;
+        public static final int Monday=0x7f05000d;
+        public static final int Saturday=0x7f050012;
+        public static final int Sunday=0x7f050013;
+        public static final int Thursday=0x7f050010;
+        public static final int Tuesday=0x7f05000e;
+        public static final int Wednesday=0x7f05000f;
         public static final int action_settings=0x7f050001;
         public static final int addProject=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int blank_text=0x7f050014;
+        public static final int cancel=0x7f05001d;
         public static final int delProject=0x7f050005;
+        public static final int done=0x7f05001c;
         public static final int end_date=0x7f050007;
         public static final int hello_world=0x7f050002;
+        public static final int hours_time=0x7f050008;
+        public static final int index_1=0x7f050017;
+        public static final int index_2=0x7f050018;
+        public static final int index_3=0x7f050019;
+        public static final int index_4=0x7f05001a;
+        public static final int index_5=0x7f05001b;
+        public static final int minites_time=0x7f050009;
+        public static final int project_name=0x7f05000a;
+        public static final int select_days_of_week=0x7f05000c;
+        public static final int set_end_date=0x7f050016;
+        public static final int set_start_date=0x7f050015;
         public static final int start_date=0x7f050006;
+        public static final int time_spent_per_day=0x7f05000b;
         public static final int title_activity_create_project=0x7f050004;
     }
     public static final class style {
