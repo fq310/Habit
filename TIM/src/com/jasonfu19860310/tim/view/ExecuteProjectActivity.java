766f020b0745792983878514f70e8e3df6197d98
jason <fuqiang.dev@gmail.com>
2013-08-09 13:23:54 +0800

    revise execute project activity

--- TIM/src/com/jasonfu19860310/tim/view/ExecuteProjectActivity.java
+++ TIM/src/com/jasonfu19860310/tim/view/ExecuteProjectActivity.java
@@ -1,10 +1,13 @@
 package com.jasonfu19860310.tim.view;
 
+
+
 import com.jasonfu19860310.tim.R;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class ExecuteProjectActivity extends Activity {
 
@@ -20,5 +23,15 @@ public class ExecuteProjectActivity extends Activity {
 		getMenuInflater().inflate(R.menu.execute_project, menu);
 		return true;
 	}
+	
+	public void onStartClicked(View view) {
+	 }
+	
+	public void onClearClicked(View view) {
+	 }
+	
+	public void onSaveClicked(View view) {
+		int i = 0;
+	 }
 
 }
