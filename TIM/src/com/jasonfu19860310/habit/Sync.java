7e9fb61ace543951eee309792ad44d970b4faafc
fq310 <fuqiang.dev@gmail.com>
2014-05-05 17:18:47 +0800

    update backup activity: remove icon

--- TIM/src/com/jasonfu19860310/habit/Sync.java
+++ TIM/src/com/jasonfu19860310/habit/Sync.java
@@ -22,6 +22,7 @@ public class Sync extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sync);
+		getActionBar().setDisplayShowHomeEnabled(false);
 		initialButton();
 	}
 
